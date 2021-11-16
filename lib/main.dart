6a38a9ba1ca243a21122b353f1565127b60c428d
mriodev <malindudarshana2@gmail.com>
2021-11-16 10:25:49 +0530

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'new shoes',
+      amount: 69.56,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't1',
+      title: 'new shoes',
+      amount: 69.56,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,6 +30,12 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final titleController = TextEditingController();
+  final amountController =
+      TextEditingController(); //text editing controller is a class provided by flutter class
+  //this should assign to the textfield of ur app
+  //statelessWidget data is immutable it means user cannot change the transaction data in runtime
+  final List<Transaction> transactions = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,18 +44,98 @@ class MyHomePage extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Card(
-              color: Colors.blueGrey.shade700,
-              child: Container(
-                width: double.infinity, //full size of container width
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blueGrey.shade700,
+                //full size of container width
                 child: Text('CHART!'),
                 //container is the most universal styling positioning sizing widget
                 //child from card widget
+                elevation: 5,
               ),
-              elevation: 5, //make strong drop shadow for card section
+              //make strong drop shadow for card section
             ),
-            Card(
-              child: Text('LIST OF TX'),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                    child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      )),
+                      // margin for container, container mean collection of things
+                      // enum mean pre define values or fixed set of variables
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        '\$${tx.amount}',
+                        /* tx.amount.toString(),*/
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple),
+                      ),
+                    ),
+                    Card(
+                      elevation: 5,
+                      child: Container(
+                        padding: EdgeInsets.all(0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: <Widget>[
+                            TextField(
+                              decoration: InputDecoration(labelText: 'Title'),
+                              controller: titleController,
+                              // onChanged: (val) => titleInput = val,
+                            ),
+                            TextField(
+                              decoration: InputDecoration(labelText: 'Amount'),
+                              controller: amountController,
+                              //onChanged: (val) => amountInput = val,
+                            ),
+                            FlatButton(
+                              child: Text('Add Transaction'),
+                              textColor: Colors.purple,
+                              onPressed: () {
+                                print(titleController.text);
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Column(
+                      //crossAxisAlignment: CrossAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      //adjust the alignmnet on column of text
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          DateFormat('yyyy-MM-dd').format(tx.date),
+                          /*to make bettar date widget
+                          should insert intel dependencies into the yaml*/
+
+                          // tx.date.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    )
+                  ],
+                ));
+              }).toList(),
             ),
           ],
         ));
---
+++ lib/transaction.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+//as u can see below is the costructor method of transaction classthere is no return type
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date,
+  });
+}
